6a1ff25ca7b76039a92592460e9d72c3e8ef0beb
Kuldeep Sharma <kul123sharma@gmail.com>
2018-09-14 16:41:49 +0530

    Solution-Roster-@hellokuldeep-1.1

--- RosterManagement/Roster.cs
+++ RosterManagement/Roster.cs
@@ -13,33 +13,32 @@ namespace RosterManagement
             _roster = new Dictionary<int, List<String>>();
         }
         
-        /// <summary>
-        /// Should be able to Add Student to a Particular Wave
-        /// </summary>
-        /// <param name="cadet">Refers to the name of the Cadet</param>
-        /// <param name="wave">Refers to the Wave number</param>
-        public void Add(string cadet, int wave)
-        {
+        public void Add(string cadet, int wave){
+            if(!_roster.ContainsKey(wave)){
+                _roster.Add(wave,new List<string>());
+            }
+            _roster[wave].Add(cadet);
         }
 
-        /// <summary>
-        /// Returns all the Cadets in a given wave
-        /// </summary>
-        /// <param name="wave">Refers to the wave number from where cadet list is to be fetched</param>
-        /// <returns>List of Cadet's Name</returns>
-        public List<string> Grade(int wave)
-        {
-            var list = new List<string>();
+        public List<string> Grade(int wave){
+            List<String> list = new List<string>();
+            if(_roster.ContainsKey(wave)){
+                list = _roster[wave];
+            }
+            list.Sort();
             return list;
         }
 
-        /// <summary>
-        /// Return all the cadets in the CodeSchool
-        /// </summary>
-        /// <returns>List of Cadet's Name</returns>
-        public List<string> Roster()
-        {
-            var cadets = new List<string>();
+        public List<string> Roster(){
+            List<String> cadets = new List<string>();
+            List<int> list = _roster.Keys.ToList();
+            list.Sort();
+            foreach (int i in list)  { 
+                _roster[i].Sort();
+                foreach(string temp in _roster[i]){
+                    cadets.Add(temp);
+                }
+            }
             return cadets;
         }
     }
